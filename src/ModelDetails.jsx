60f53a0d83044b5ab990de0c4cd843081b4bb55f
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-22 21:28:47 +0200

    import proptypes

--- src/ModelDetails.jsx
+++ src/ModelDetails.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ModelDetails extends Component {
+  
   render() {
     return (
       <div>
