86cdf048b14b91ec0ac32e827896f040fca942b5
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-22 13:31:18 +0200

    add the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import './App.css';
 
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
+
 function App() {
   return (
     <div className="App">
@@ -8,5 +35,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
