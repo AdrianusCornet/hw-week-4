f7a9f160593b561b336238a9e71a9e3faeccbf01
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-22 12:42:07 +0200

    conect provider to the App

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import store from './store'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  
-<App />, document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
