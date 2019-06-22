c0a2de94768d0ba1d3d8ba8350fccf69b0ca288a
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-22 21:31:03 +0200

    chek proptypes

--- src/ModelDetails.jsx
+++ src/ModelDetails.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ModelDetails extends Component {
-  
+  static propTypes = {
+    model: PropTypes.object.isRequired,
+  }
+
   render() {
     return (
       <div>
